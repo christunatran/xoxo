665abf50b151e1bc5f562d18ebc6e1cb6f814369
christinatran <christinatran@ibm.com>
2023-02-13 18:47:55 -0500

    changed title

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>muaHAHAHAHHAHAHAHAHAH!!!</title>
+        <title>valentines day card 4 u ❤ </title>
         <meta
           name="description"
           content="The Comet React SDK lets you launch new Solana NFT collections in your app!"
